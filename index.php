010b05eb42d2de42a5433224cb7dbb8ed09d539c
saputraregi <saputra.regi@widyatama.ac.id>
2025-06-14 13:04:31 +0700

    Initial commit: add To-Do List app

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
     <head>
         <title>Aplikasi To-Do List Sederhana</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="assets/style.css">
     </head>
     <body>
         <div class="container mt-5">    
